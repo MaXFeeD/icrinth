9ce9292f0758b7146e9a2193372ce3be56c57cd4
MaXFeeD <maksfeed.mf@gmail.com>
2025-06-28 11:33:24 +0300

    Temporary removed advertisements

--- apps/frontend/src/components/ui/AdPlaceholder.vue
+++ apps/frontend/src/components/ui/AdPlaceholder.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ad-parent relative mb-3 flex w-full justify-center rounded-2xl bg-bg-raised">
+  <!-- div class="ad-parent relative mb-3 flex w-full justify-center rounded-2xl bg-bg-raised">
     <div class="flex max-h-[250px] min-h-[250px] min-w-[300px] max-w-[300px] flex-col gap-4 p-6">
       <p class="m-0 text-2xl font-bold text-contrast">75% of ad revenue goes to creators</p>
     </div>
@@ -8,9 +8,9 @@
     >
       <div id="icmods-rail-1" />
     </div>
-  </div>
+  </div -->
 </template>
-<script setup>
+<!-- script setup>
 useHead({
   link: [
     {
@@ -107,4 +107,4 @@ iframe[id^="google_ads_iframe"] {
     display: none;
   }
 }
-</style>
+</style -->
