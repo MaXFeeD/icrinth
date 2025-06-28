fd42b52f598b672a1f0f8e024c2c05140b49ed9c
MaXFeeD <maksfeed.mf@gmail.com>
2025-06-28 11:50:14 +0300

    Updated hcaptcha sitekey

--- apps/frontend/src/components/ui/HCaptcha.vue
+++ apps/frontend/src/components/ui/HCaptcha.vue
@@ -41,7 +41,7 @@ defineExpose({
   <div
     id="h-captcha"
     class="h-captcha"
-    data-sitekey="4a7a2c80-68f2-4190-9d52-131c76e0c14e"
+    data-sitekey="41468818-ab70-47ba-9968-0e209aae2540"
     :data-theme="$theme.active === 'light' ? 'light' : 'dark'"
     data-callback="hCaptchaUpdateToken"
   ></div>
