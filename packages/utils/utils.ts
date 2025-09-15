7eb9c2f4f439b243bb439f04380e770f01c3781c
MaXFeeD <maksfeed.mf@gmail.com>
2025-09-15 18:46:23 +0300

    Frontend modpack and mod metadata inferring, removed packing of java datapacks

--- packages/utils/utils.ts
+++ packages/utils/utils.ts
@@ -281,9 +281,9 @@ export const fileIsValid = (file, validationOptions) => {
 export const acceptFileFromProjectType = (projectType) => {
   switch (projectType) {
     case 'mod':
-      return '.jar,.zip,.litemod,application/java-archive,application/x-java-archive,application/zip'
+      return '.zip,.icmod,application/zip'
     case 'modpack':
-      return '.mrpack,application/x-innercore-modpack+zip,application/zip'
+      return '.mrpack,.icmod,.icpack,application/x-innercore-modpack+zip,application/zip'
     default:
       return '*'
   }
