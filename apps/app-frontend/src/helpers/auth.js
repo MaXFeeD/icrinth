7a0221c962f2c1ba4db1edb6623313ec0868512d
MaXFeeD <maksfeed.mf@gmail.com>
2025-10-09 17:40:38 +0300

    Migrating to auth, mr_auth is a little bit unexpected

--- apps/app-frontend/src/helpers/auth.js
+++ apps/app-frontend/src/helpers/auth.js
@@ -6,13 +6,13 @@
 import { invoke } from '@/composables/bridge'
 
 export async function login() {
-  return await invoke('plugin:mr-auth|modrinth_login')
+  return await invoke('plugin:auth|login')
 }
 
 export async function logout() {
-  return await invoke('plugin:mr-auth|logout')
+  return await invoke('plugin:auth|logout')
 }
 
 export async function get() {
-  return await invoke('plugin:mr-auth|get')
+  return await invoke('plugin:auth|get')
 }
